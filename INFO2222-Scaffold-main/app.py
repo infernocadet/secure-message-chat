989cbb6f306f00013cab5b50a20d0388b873c6bf
Marc <caramoanmarc@gmail.com>
2024-04-14 23:11:47 +1000

    done

--- INFO2222-Scaffold-main/app.py
+++ INFO2222-Scaffold-main/app.py
@@ -266,4 +266,4 @@ def reject_friend_request():
         
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True, ssl_context=("certs/cert.pem", "certs/key.pem"))
+    app.run(host="0.0.0.0", port=5000, debug=True, ssl_context=("cert.pem", "key.pem"))
