09c5ec168b9e08846869ef737e53f784fc012deb
Marc <caramoanmarc@gmail.com>
2024-04-17 16:30:02 +1000

    cleaned file - removed all password hashing and salting. raw password in place

--- INFO2222-Scaffold-main/db.py
+++ INFO2222-Scaffold-main/db.py
@@ -6,10 +6,7 @@ database file, containing all the logic to interface with the sql database
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from models import *
-from argon2 import PasswordHasher
 from pathlib import Path
-
-ph = PasswordHasher()
 
 # creates the database directory
 Path("database") \
@@ -24,9 +21,9 @@ engine = create_engine("sqlite:///database/main.db", echo=False)
 Base.metadata.create_all(engine)
 
 # inserts a user to the database
-def insert_user(username: str, password: str, salt: str):
+def insert_user(username: str, password: str):
     with Session(engine) as session:
-        user = User(username=username, password=password, salt=salt)
+        user = User(username=username, password=password)
         session.add(user)
         session.commit()
 
@@ -34,15 +31,3 @@ def insert_user(username: str, password: str, salt: str):
 def get_user(username: str):
     with Session(engine) as session:
         return session.get(User, username)
-
-# Verify password of user
-def verify_password(hashed_password: str, password: str):
-    try:
-        ph.verify(hashed_password, password)
-        return True
-    except:
-        return False
-
-##########################################################
-# NOTE: May need to check if password needs to be rehashed
-##########################################################
